155abc4d7504f7eb9a37e79112225b3b10c7dbd4
yashwanthgh <yugnasura@gmail.com>
2024-04-03 12:51:59 +0530

    Displaying

---
+++ ReservationSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem
+{
+    public class ReservationSystem
+    {
+        public void Display()
+        {
+            Console.WriteLine("Welcome to Hotel Registration System");
+        }
+    }
+}
